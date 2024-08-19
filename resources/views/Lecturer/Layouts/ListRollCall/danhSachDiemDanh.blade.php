fe1a4b6c60b6b3b28aa78211395e30954d54a3dd
hungne2k3 <104420895+hungne2k3@users.noreply.github.com>
2024-08-19 15:27:24 +0700

    sửa UI table

--- resources/views/Lecturer/Layouts/ListRollCall/danhSachDiemDanh.blade.php
+++ resources/views/Lecturer/Layouts/ListRollCall/danhSachDiemDanh.blade.php
@@ -14,8 +14,13 @@
                 <hr class="seperator">
                 <!-- Date-picker -->
                 <div class="flex content-center justify-end" style="gap: 10px;">
-                    <label for="dateofbirth" style="font-size: 14px;">Ngày điểm danh:</label>
-                    {{-- <input type="date" name="dateofbirth" id="dateofbirth"> --}}
+                    <label for="dateofbirth"
+                        style="font-size: 14px; font-size: 14px;
+                                font-weight: 600;
+                                color: #0C8281;">
+                        Ngày điểm
+                        danh:
+                    </label>
                     <input type="date" id="myID" name="selected_date" class="flatpick input-date ct-shape"
                         value="" placeholder="{{ $currentDate->format('Y-m-d') }}" readonly
                         style="padding: 10px;
@@ -28,14 +33,14 @@
                     <table class="table" style="font-size: 14px; position: relative;">
                         <thead>
                             <tr>
-                                <th>STT</th>
+                                <th style="width: 4%">STT</th>
                                 <th>Mã sinh viên</th>
-                                <th>Họ và tên</th>
-                                <th>Môn học/Module</th>
-                                <th>Tên lớp</th>
-                                <th>Ngày điểm danh</th>
-                                <th>Ca</th>
-                                <th>Tiết</th>
+                                <th style="width: 12%">Họ và tên</th>
+                                <th style="width: 14%">Môn học/Module</th>
+                                <th style="width: ">Tên lớp</th>
+                                <th style="width: ">Ngày điểm danh</th>
+                                <th style="width: 3%">Ca</th>
+                                <th style="width: 7%">Tiết</th>
                                 <th>Số tiết đi muộn</th>
                                 <th colspan="2">Ghi chú</th>
                             </tr>
@@ -332,36 +337,6 @@
                                 </td>
                             </tr>
                             <tr class="tr__title">
-                                <td>1</td>
-                                <td>2109610335</td>
-                                <td>Phạm Xuân Huy</td>
-                                <td>An toàn và bảo mật thông tin</td>
-                                <td>1621CNT03</td>
-                                <td>15/07/2024</td>
-                                <td>Sáng</td>
-                                <td>Tiết 1-5</td>
-                                <td>
-                                    <div class="search-option">
-                                        <select name="search__select-option justify-center" id="search__select-option"
-                                            class="select-option search__select-option">
-                                            <option value="1">-- Chọn số tiết --</option>
-                                            <option value="2">Đi học đầy đủ - 0</option>
-                                            <option value="3">Vắng - 1 tiết</option>
-                                            <option value="3">Vắng - 2 tiết</option>
-                                            <option value="3">Vắng - 3 tiết</option>
-                                            <option value="3">Vắng - 4 tiết</option>
-                                            <option value="3">Vắng - 5 tiết</option>
-                                            <option value="3">Nghỉ có lí do</option>
-                                            <option value="3">Nghỉ không lí do</option>
-                                        </select>
-                                    </div>
-                                </td>
-                                <td colspan="2">
-                                    <input type="text" value="Vào học muộn 45 phút" class="table__input">
-                                </td>
-                            </tr>
-                            <tr class="tr__title">
-                                <td>Xin chao cac ban</td>
                                 <td>1</td>
                                 <td>2109610335</td>
                                 <td>Phạm Xuân Huy</td>
